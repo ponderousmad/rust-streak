c1dcceb7d436ee295780a89ecc48a6de014bf3be
Adrian Smith <agnomen@gmail.com>
2016-10-04 21:27:23 -0400

    Sphere WIPs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,23 @@
-mod point;
+pub mod point;
+pub mod sphere;
 
 extern crate cgmath;
 
 #[cfg(test)]
 mod tests {
     use point::Point;
+    use sphere::Sphere;
 
     use std::f64;
 
     use cgmath::Matrix4;
     use cgmath::Vector4;
+    use cgmath::Point3;
     use cgmath::Transform;
 
     type Mat4 = Matrix4<f64>;
     type Vec4 = Vector4<f64>;
+    type P3 = Point3<f64>;
 
     #[test]
     fn apply_matrix() {
@@ -28,5 +32,12 @@ mod tests {
         let p = Point { x: 1.0, y: 1.0, z: 1.0 };
         let still_p = origin + p;
         assert!(still_p.x == p.x);
+    }
+
+    #[test]
+    fn sphere_volume() {
+        let center = P3::new(1.0, 1.0, 1.0);
+        let s = Sphere::new(center, 10.0);
+        assert!(s.volume() == (4000.0 * f64::consts::PI / 3.0));
     }
 }
